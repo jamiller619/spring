ee339ceb0bd807ee3601f7fd003362412ce6223f
Jeff Miller <jamiller619@gmail.com>
2016-10-09 09:20:18 -0400

    updated default background color, updated clock assets

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Spring
 An elegant new tab page for Google Chrome
 
-Tired with most of the current New Tab page offerings in the Chrome Extension gallery I decided to create my own.
-
-There are only two features of Spring:
-- Full size background images from Unsplash tailored to the resolution of your monitor
-- Analog clock inspired by the beautiful timepiece designs of Ziiiro 
+Featuring:
+- Full size background image from Unsplash tailored to the resolution of your monitor
+- Analog clock inspired by Ziiiro
--- assets/scripts/spring.js
+++ assets/scripts/spring.js
@@ -1,8 +1,6 @@
 (function(window, document) {
   "use strict";
 
-  var Spring = Spring || {};
-
   if (!String.prototype.format) {
     String.prototype.format = function() {
       var args = arguments;
@@ -15,10 +13,22 @@
     };
   }
 
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  var Spring = Spring || {};
+
   Spring.Unsplash = {
+    url: 'https://source.unsplash.com/{0}',
     init: function() {
-      var unsplashUrl = "https://source.unsplash.com/{0}/daily",
-          bgUrl = unsplashUrl.format(window.innerWidth + 'x' + window.innerHeight);
+      var bgUrl = this.url.format(window.innerWidth + 'x' + window.innerHeight);
 
       document.body.style.backgroundImage = 'url({0})'.format(bgUrl);
     }
@@ -26,56 +36,114 @@
 
   Spring.TimePiece = (function() {
 
+    var currentMinutes;
+    
     var secondsInHour = 60 * 60,
-        secondsInDay = secondsInHour * 12;
+        secondsInDay = secondsInHour * 12,
+        timeClasses = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+
+    var timePiece = document.getElementById('timepiece'),
+        digital = document.getElementById('digital');
 
     var els = {
-      minute: document.getElementById('minute'),
-      hour: document.getElementById('hour'),
-      timePiece: document.getElementById('timepiece')
+      minute: timePiece.querySelector('.minutes'),
+      hour: timePiece.querySelector('.hour'),
+      digitalHours: digital.querySelector('.hour'),
+      digitalMinutes: digital.querySelector('.minutes'),
+      digitalAmPm: digital.querySelector('.am-pm'),
+      digitalDate: digital.querySelector('.date')
     };
 
     function setRotation(el, deg) {
       el.style.transform = 'rotate({0}deg)'.format(deg);
     }
 
-    function drawClock() {
-      var now = new Date(),
-          _hours = now.getHours(),
-          seconds = now.getSeconds(),
-          minutes = now.getMinutes(),
-          hours = _hours > 12 ? _hours - 12 : _hours,
-          secondsInHour = 60 * 60,
-          secondsInDay = secondsInHour * 12,
-          minutesInSeconds = (minutes + (seconds/60)) * 60,
+    function drawText(word) {
+      var fragment = document.createDocumentFragment();
+
+      for(var i=0; i<word.length; i++) {
+        var span = document.createElement('span');
+        span.textContent = word[i];
+        span.className = timeClasses[~~word[i]];
+        fragment.appendChild(span);
+      }
+      return fragment;
+    }
+
+    function emptyNode(node) {
+      while(node.lastChild) {
+        node.removeChild(node.lastChild);
+      }
+      return node;
+    }
+
+    function drawTime(hours, minutes, seconds) {
+      var minutesInSeconds = (minutes + (seconds/60)) * 60,
           hoursInSeconds = (hours + (minutes/60)) * secondsInHour,
-          amPm = _hours > 11 ? 'PM' : 'AM',
-          displayMinutes = minutes.length == 1 ? '0' + minutes : minutes;
+          amPm = hours > 11 ? 'PM' : 'AM',
+          displayHours = (hours % 12 == 0 ? 12 : hours % 12).toString(),
+          displayMinutes = minutes >= 10 ? minutes.toString() : '0' + minutes;
 
       setRotation(els.minute, (minutesInSeconds / secondsInHour) * 360);
       setRotation(els.hour, (hoursInSeconds / secondsInDay) * 360);
 
-      els.timePiece.setAttribute('title', '{0}:{1} {2}'.format(hours, displayMinutes, amPm));
+      if(minutes != currentMinutes) {
+        emptyNode(els.digitalHours).appendChild(drawText(displayHours));
+        emptyNode(els.digitalMinutes).appendChild(drawText(displayMinutes));
 
-      requestAnimationFrame(drawClock);
+        els.digitalAmPm.textContent = amPm;
+
+        currentMinutes = minutes;
+      }
+    }
+
+    function drawDate(day, date, month) {
+      var months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
+          days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+          displayDate = '{0}, {1} {2}'.format(days[day], months[month], date);
+          
+      els.digitalDate.textContent = displayDate;
     }
 
     function init() {
-      requestAnimationFrame(drawClock);
+      var now = new Date(),
+          seconds = now.getSeconds(),
+          minutes = now.getMinutes(),
+          hours = now.getHours(),
+          day = now.getDay(),
+          date = now.getDate(),
+          month = now.getMonth();
+
+      drawTime(hours, minutes, seconds);
+      drawDate(day, date, month);
+
+      requestAnimationFrame(init);
     }
 
     return {
-      init: init
+      init: init,
+      drawCustomTime: function(hours, minutes) {
+        drawTime(hours, minutes, 0);
+
+        var d = new Date();
+        drawDate(d.getDay(), d.getDate(), d.getMonth());
+      }
     }
   })();
 
   Spring.init = function() {
+    var param = getParameterByName('t');
+
     this.Unsplash.init();
-    this.TimePiece.init();
+
+    if(param) {
+      var time = param.split(':');
+      this.TimePiece.drawCustomTime(~~time[0], ~~time[1]);
+    } else {
+      this.TimePiece.init();
+    }
   };
 
   Spring.init();
-
-  window.Spring = Spring;
 
 })(window, document);
